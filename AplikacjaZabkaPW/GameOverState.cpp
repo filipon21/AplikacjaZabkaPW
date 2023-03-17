9142378754be97af829b2a2f6fefa1274ad0a031
Filip Onufrowicz <filip.onufrowicz@orange.com>
2023-03-17 18:13:18 +0100

    Added new states

---
+++ AplikacjaZabkaPW/GameOverState.cpp
@@ -0,0 +1,17 @@
+#include "GameOverState.h"
+
+void GameOverState::init()
+{
+}
+
+void GameOverState::handleInput()
+{
+}
+
+void GameOverState::update(float dt)
+{
+}
+
+void GameOverState::draw(float dt)
+{
+}
